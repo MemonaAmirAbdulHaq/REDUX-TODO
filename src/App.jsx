4b4f8d06892167884034a817885db1ed223e9da7
Memona Amir <157165172+MemonaAmirAbdulHaq@users.noreply.github.com>
2024-09-19 18:44:53 +0500

    deploy react to github

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1>learn about redux toolkit</h1>
+     <AddTodo/>
+    
+    <Todos/>
+    </>
+  )
+}
+
+export default App
